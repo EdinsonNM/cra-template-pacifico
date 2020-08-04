bfba878e086660903c8713ada99a10723f603ebd
Edinson Nuñez More <nmedinson@gmail.com>
2020-08-03 19:34:29 -0500

    fix template dependencies

--- template/src/index.js
+++ template/src/index.js
@@ -1,19 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {defineCustomElements} from "@pacificoseguros/pixie/loader";
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 import "@pacificoseguros/pixie/variables.css";
-import "./assets/styles/index.css";
-import App from "./containers/app/app";
-import * as serviceWorker from "./serviceWorker";
 import "./assets/styles/theme/theme.scss";
-//ReactDOM.render(<App />, document.getElementById("root"));
-const rootElement = document.getElementById("root");
 
-if (rootElement.hasChildNodes()) {
-    ReactDOM.hydrate(<App />, rootElement);
-    // If you want your app to work offline and load faster, you can change
-    // unregister() to register() below. Note this comes with some pitfalls.
-    // Learn more about service workers: https://bit.ly/CRA-PWA
-    serviceWorker.register();
-} else {
-    ReactDOM.render(<App />, rootElement);
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+defineCustomElements(window);
